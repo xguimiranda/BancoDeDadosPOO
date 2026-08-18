a541cfd23d109ab408da98d63ce13e325c17ed89
xguimiranda <saguilherme16@gmail.com>
2026-08-18 11:41:17 -0300

    FEAT: creating inserir method

--- src/main/java/dao/VendedorDAO.java
+++ src/main/java/dao/VendedorDAO.java
@@ -7,6 +7,16 @@ import java.util.List;
 public class VendedorDAO implements GenericDAO<Vendedor, Integer> {
     @Override
     public void inserir(Vendedor entidade) {
+        String sql = "insert into java_vendedor(nome) values(?)";
+
+        try(Connection connection = ConnectionFactory.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setString(1, entidade.getNome());
+            ps.execute();
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }
 
     }
 
