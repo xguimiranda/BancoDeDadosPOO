80868dffa2d26290cafa1ad9fd61d3d39810e1b4
xguimiranda <saguilherme16@gmail.com>
2026-08-18 11:23:37 -0300

    FEAT: VendedorDAO.java

---
+++ src/main/java/dao/VendedorDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import model.Vendedor;
+
+import java.util.List;
+
+public class VendedorDAO implements GenericDAO<Vendedor, Integer> {
+    @Override
+    public void inserir(Vendedor entidade) {
+
+    }
+
+    @Override
+    public List<Vendedor> listar() {
+        return null;
+    }
+}
