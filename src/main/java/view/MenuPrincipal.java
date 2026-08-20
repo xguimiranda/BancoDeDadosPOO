f437d6066d5852e25bd94fa855b73e267cac4bf0
xguimiranda <saguilherme16@gmail.com>
2026-08-20 09:37:28 -0300

    FEAT: MenuPrincipal.java

---
+++ src/main/java/view/MenuPrincipal.java
@@ -0,0 +1,15 @@
+package view;
+import static javax.swing.JOptionPane.*;
+public class MenuPrincipal {
+    public static void menu() {
+        String[] item = {"Vendedor", "Venda", "Finalizar"};
+        String opcao;
+        do {
+            opcao = (String) (showInputDialog(null, "Selecione uam opção", "*** MENU PRINCIPAL ***", INFORMATION_MESSAGE, null, item, item[0]));
+            switch (opcao.toLowerCase()){
+                case "vendedor" -> new MenuVendedor().menu();
+                case "Venda" -> new MenuVenda().menu();
+            }
+        } while(!opcao.toLowerCase().equals("finalizar"));
+    }
+}
