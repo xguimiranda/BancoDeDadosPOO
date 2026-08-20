5b5eec8485a1eda9fb2a4a0f9161f8ee9cb618a7
xguimiranda <saguilherme16@gmail.com>
2026-08-20 09:42:00 -0300

    FEAT: Imports

--- src/main/java/view/MenuVenda.java
+++ src/main/java/view/MenuVenda.java
@@ -1,5 +1,10 @@
 package view;
 
+import model.Vendedor;
+
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
+import static javax.swing.JOptionPane.showInputDialog;
+
 public class MenuVenda {
     public void menu(){
 
