0ce19b2c497e9fb4380c8cd9648b97994c49cb3b
xguimiranda <saguilherme16@gmail.com>
2026-08-20 09:37:46 -0300

    FEAT: MenuVendedor.java

---
+++ src/main/java/view/MenuVendedor.java
@@ -0,0 +1,32 @@
+package view;
+
+import DAO.VendedorDAO;
+import model.Vendedor;
+
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class MenuVendedor {
+    public void menu() {
+        String[] item = {"Inserir", "Pesquisar", "Listar", "Atualizar", "Excluir", "Sair"};
+        String opcao;
+        do {
+            opcao = (String) (showInputDialog(null, "Selecione uam opção", "*** MENU VENDEDOR ***", INFORMATION_MESSAGE, null, item, item[0]));
+            switch (opcao.toLowerCase()){
+                case "inserir" -> inserir();
+                case "listar" -> listar();
+            }
+        } while(!opcao.toLowerCase().equals("sair"));
+    }
+
+    private void listar() {
+
+    }
+
+    private void inserir() {
+        Vendedor vendedor = new Vendedor();
+        String nome = showInputDialog("Nome");
+        vendedor.setNome(nome);
+        new VendedorDAO().inserir(vendedor);
+    }
+}
