eef6f7cc3ac65b143eca603a19d14e7344bcc923
xguimiranda <saguilherme16@gmail.com>
2026-08-20 09:37:53 -0300

    FEAT: VendedorDAO.java

--- src/main/java/dao/VendedorDAO.java
+++ src/main/java/dao/VendedorDAO.java
@@ -1,10 +1,18 @@
-package dao;
+package DAO;
 
+import dao.GenericDAO;
+import factory.ConnectionFactory;
 import model.Vendedor;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VendedorDAO implements GenericDAO<Vendedor, Integer> {
+
     @Override
     public void inserir(Vendedor entidade) {
         String sql = "insert into java_vendedor(nome) values(?)";
@@ -13,15 +21,21 @@ public class VendedorDAO implements GenericDAO<Vendedor, Integer> {
             PreparedStatement ps = connection.prepareStatement(sql)){
             ps.setString(1, entidade.getNome());
             ps.execute();
-
-        }catch (SQLException e){
-            System.out.println(e);
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir Vendedor: " + e.getMessage());
         }
-
     }
 
     @Override
     public List<Vendedor> listar() {
-        return null;
+        String sql = "select * from java_vendedor";
+        List<Vendedor> listaVendedor = new ArrayList<>();
+        try(Connection connection = ConnectionFactory.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql)){
+            ResultSet rs = ps.executeQuery();
+        } catch (SQLException e) {
+            System.out.println("Erro ao listar Vendedor" + e.getMessage());
+        }
+        return  listaVendedor;
     }
 }
