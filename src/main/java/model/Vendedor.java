5e8eb64051b8b616b2e37696628e539423449409
xguimiranda <saguilherme16@gmail.com>
2026-08-20 09:49:21 -0300

    FEAT: toString

--- src/main/java/model/Vendedor.java
+++ src/main/java/model/Vendedor.java
@@ -19,4 +19,9 @@ public class Vendedor {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    @Override
+    public String toString() {
+        return id + " | " + nome;
+    }
 }
